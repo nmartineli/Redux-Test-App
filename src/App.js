d0497570d4991c07c94da607fb83951a028828c2
unknown <nataliamartineli@gmail.com>
2022-04-29 19:35:51 -0300

    Finished Redux test

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import Counter from './Counter';
 import './styles.css';
 
 function App() {
+	//use selector é um hook do react-redux que usamos para selecionar o estado. conseguimos pegar dentro de state.NOME DO REDUCER.NOME DO ESTADO
 	const count = useSelector((state) => state.counter.count);
+
+	const Voters = ['Natalia Martineli', 'Bianca Banzato', 'Patrícia Pereira'];
 	return (
 		<div className="App">
 			<h1>Redux made easy</h1>
-			<h2> The count is {count} </h2>
-			<Counter />
+			<h2> The total votes is {count} </h2>
+			{Voters.map((voter) => (
+				<Counter voter={voter} />
+			))}
 		</div>
 	);
 }
--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,23 +1,29 @@
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { decrement, increment } from './redux/ducks/counter';
 
-export default function Counter() {
-	//use selector é um hook do react-redux que usamos para selecionar o estado. conseguimos pegar dentro de state.NOME DO REDUCER.NOME DO ESTADO
+export default function Counter(props) {
+	const { voter } = props;
 
 	//hook para usar o dispatch que dispara o reducer
 	const dispatch = useDispatch();
 
+	const [votes, setVotes] = useState(0);
+
 	const handleIncrement = () => {
 		dispatch(increment());
+		setVotes(votes + 1);
 	};
 
 	const handleDecrement = () => {
 		dispatch(decrement());
+		setVotes(votes - 1);
 	};
 
 	return (
 		<div style={{ backgroundColor: 'grey', margin: '10px' }}>
+			<h2>{voter}</h2>
+			<h3>Votes: {votes}</h3>
 			<div style={{ display: 'flex', justifyContent: 'center' }}>
 				<button onClick={handleIncrement}>Increment</button>
 				<button onClick={handleDecrement}>Decrement</button>
